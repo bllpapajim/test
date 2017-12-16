b9c1d466e119d3d98111a7dc2c7824b9c578e1e7
bllpapajim <bllpapajim@gmail.com>
2017-12-16 02:59:29 +0200

    Add files via upload

--- www/kw/script_jq.js
+++ www/kw/script_jq.js
@@ -5,13 +5,13 @@
             var username = $("#username").val();
             var passwd = $("#passwd").val();
 	    var mobile =1;
-            $("#stage2").load('https://kapaweb.gr/controlpanel/login-exec.php', {"passwd": passwd, "username": escape(username), "mobile":mobile} );
+            $("#stage2").load('https://kapaweb.gr/app/login-exec.php', {"passwd": passwd, "username": escape(username), "mobile":mobile} );
         } else {
             var username = $("#username").val();
             var passwd = $("#passwd").val();
             var mobile =1;
             if(username !== '' && passwd!== '') {
-                $("#stage2").load('https://kapaweb.gr/controlpanel/login-exec.php', {"passwd": passwd, "username": escape(username), "mobile":mobile} );
+                $("#stage2").load('https://kapaweb.gr/app/login-exec.php', {"passwd": passwd, "username": escape(username), "mobile":mobile} );
             }
         }
 
@@ -21,7 +21,7 @@
             var password = $("#password").val();
             var cpassword = $("#cpassword").val();
             var mobile = 1;
-            $("#stage").load('https://kapaweb.gr/controlpanel/login-exec.php', {"password":password,"cpassword":cpassword,"uname":uname, "mobile":mobile} );
+            $("#stage").load('https://kapaweb.gr/app/login-exec.php', {"password":password,"cpassword":cpassword,"uname":uname, "mobile":mobile} );
 			$("#driver").fadeIn(2000);
         });
 
@@ -33,7 +33,7 @@
             window.localStorage.setItem('user', username);
             window.localStorage.setItem('passwd', passwd);
             var mobile = 1;
-            $("#stage2").load('https://kapaweb.gr/controlpanel/login-exec.php', {"passwd":passwd,"username":escape(username),"mobile":mobile} );
+            $("#stage2").load('https://kapaweb.gr/app/login-exec.php', {"passwd":passwd,"username":escape(username),"mobile":mobile} );
 			$("#driver2").fadeIn(2000);
         });
         var passwd = $("#passwd").val();
@@ -42,5 +42,5 @@
         window.localStorage.setItem('user', username);
         window.localStorage.setItem('passwd', passwd);
         var mobile = 1;
-        $("#stage2").load('https://kapaweb.gr/controlpanel/login-exec.php', {"passwd":passwd,"username":escape(username),"mobile":mobile} );
+        $("#stage2").load('https://kapaweb.gr/app/login-exec.php', {"passwd":passwd,"username":escape(username),"mobile":mobile} );
     });
